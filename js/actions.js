90164fb6427ba720035277b348454aa073abb45c
Rimantas <belauzas@yahoo.com>
2019-04-08 11:32:38 +0300

    Updates for teamwork

--- js/actions.js
+++ js/actions.js
@@ -1,2 +1,47 @@
 "use strict";
 
+/* HEADER */
+
+
+
+/* HERO */
+
+
+
+/* CLIENT LOGOS */
+
+
+
+/* ABOUT MYSELF */
+
+
+
+/* STATISTICS and SERVICE OFFERS */
+
+
+
+/* SKILLS */
+
+
+
+/* LATEST WORKS */
+
+
+
+/* JOB HISTORY */
+
+
+
+/* TESTIMONIALS */
+
+
+
+/* CONTACT ME */
+
+
+
+/* FOOTER */
+
+
+
+/* BACKT TO TOP */
