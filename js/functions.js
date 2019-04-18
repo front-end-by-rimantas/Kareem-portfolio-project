3b0dc3173419e91c9eefb9448b8155e263aaaade
donataspiragis <32620070+donataspiragis@users.noreply.github.com>
2019-04-18 07:09:45 +0300

    Header menu #4

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,57 @@ function stickToTop() {
       header.classList.remove("sticky");
     }
   }
+/* SLIDING MENU*/
+let toggleNavStatus = false;
 
+let toggleNav = function() {
+  let getSidebar = document.querySelector(".nav-sidebar");
+  let getSidebarUl = document.querySelector(".nav-sidebar ul");
+  let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+  if (toggleNavStatus === false) {
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "50%";
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = "1";
+    }
+    toggleNavStatus = true;
+  }
+  else if (toggleNavStatus === true) {
+    getSidebar.style.width = "0";
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = "0";
+    }
+    getSidebarUl.style.visibility = "hidden";
+    toggleNavStatus = false;
+  }
+}
+let toggleNavStatus2 = false;
+
+let toggleNav2 = function() {
+  let getSidebar = document.querySelector(".nav-sidebar-right");
+  let getSidebarUl = document.querySelector(".nav-sidebar-right ul");
+  let getSidebarLinks = document.querySelectorAll(".nav-sidebar-right a");
+  if (toggleNavStatus2 === false) {
+    getSidebarUl.style.visibility = "visible";
+    getSidebar.style.width = "50%";
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = "1";
+    }
+    toggleNavStatus2 = true;
+  }
+  else if (toggleNavStatus2 === true) {
+    getSidebar.style.width = "0";
+    let arrayLength = getSidebarLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = "0";
+    }
+    getSidebarUl.style.visibility = "hidden";
+    toggleNavStatus2 = false;
+  }
+}
 /* HERO */
 
 
